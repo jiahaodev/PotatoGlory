11fe5b6156f18316c36ddbe9ea7198f85bd76b53
JiahaoWu <jiahaodev@163.com>
2020-01-05 15:26:42 +0800

    18.使用自定义Inspector窗口拓展Generator

--- Assets/Scripts/Utility/Generator.cs
+++ Assets/Scripts/Utility/Generator.cs
@@ -6,6 +6,7 @@
 	功能：怪物随机生成器
 *****************************************************/
 
+using System.Collections;
 using UnityEngine;
 
 //朝向
@@ -20,8 +21,30 @@ public class Generator : MonoBehaviour
 {
     [Tooltip("多久之后开始实例化预设对象")]
     public float GenerateDelay = 2f;
+
+    [Tooltip("是否使用随机时间间隔来实例化预设对象")]
+    public bool RandomGenerateInterval = false;
+    [Tooltip("实例化预设对象的最短时间间隔")]
+    public float MinGenerateInterval;
+    [Tooltip("实例化预设对象的最长时间间隔")]
+    public float MaxGenerateInterval;
     [Tooltip("实例化预设对象的时间间隔")]
     public float GenerateInterval = 3f;
+
+    [Tooltip("是否在随机的X坐标上实例化预设对象")]
+    public bool RandomGeneratePositionX = false;
+    [Tooltip("实例化预设对象时的最小X坐标")]
+    public float MinGeneratePositionX;
+    [Tooltip("实例化预设对象时的最大X坐标")]
+    public float MaxGeneratePositionX;
+
+    [Tooltip("是否在随机的Y坐标上实例化预设对象")]
+    public bool RandomGeneratePositionY = false;
+    [Tooltip("实例化预设对象时的最小Y坐标")]
+    public float MinGeneratePositionY;
+    [Tooltip("实例化预设对象时的最大Y坐标")]
+    public float MaxGeneratePositionY;
+
     [Tooltip("预设对象的朝向")]
     public Orientation PrefabOrientation = Orientation.Right;
     [Tooltip("预设对象")]
@@ -41,14 +64,59 @@ public class Generator : MonoBehaviour
     private void Start()
     {
         // GenerateDelay秒之后第一次调用Generate函数，然后每隔GenerateInterval调用Generate函数一次
-        InvokeRepeating("Generate",GenerateDelay,GenerateInterval);
+        //InvokeRepeating("Generate",GenerateDelay,GenerateInterval);
+
+        //开启随机生成的协程
+        StartCoroutine(RandomGenerate());
     }
 
+    private IEnumerator RandomGenerate() {
+        yield return new WaitForSeconds(GenerateDelay);
+
+        while (true)
+        {
+            //确定下一次实例化预设对象的时间间隔
+            float interval = GenerateInterval;
+            if (RandomGenerateInterval)
+            {
+                interval = Random.Range(MinGenerateInterval,MaxGenerateInterval);
+            }
+            yield return new WaitForSeconds(interval);
+
+            //实例化预设对象
+            Generate();
+        }
+
+    }
+
+
+
+
     private void Generate() {
+        //随机选择要实例化的预设的下标
         int index = UnityEngine.Random.Range(0,Prefabs.Length);
 
-        GameObject prefab = Instantiate(Prefabs[index],transform.position,Quaternion.identity);
+        //确定生成位置的X坐标
+        float x = transform.position.x;
+        if (RandomGeneratePositionX)
+        {
+            x = Random.Range(MinGeneratePositionX,MaxGeneratePositionX);
+        }
 
+        //确定生成位置的Y坐标
+        float y = transform.position.y;
+        if (RandomGeneratePositionY)
+        {
+            y = Random.Range(MinGeneratePositionY,MaxGeneratePositionY);
+        }
+
+        //更新位置
+        transform.position = new Vector3(x,y,transform.position.z);
+
+        //实例化预设对象
+        GameObject prefab = Instantiate(Prefabs[index], transform.position, Quaternion.identity);
+
+        //播放粒子特效
         if (m_Priticle!=null)
         {
             m_Priticle.Play();
